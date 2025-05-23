98337338279fb2f36280b9ff6d49b1b60a9b6048
Luiza Svetoslavova <luizasvetoslavova16@gmail.com>
2025-05-23 21:19:15 +0300

    add spring boot dependencies

---
+++ backend/src/test/java/com/cryptosim/backend/CryptoTradingSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptosim.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
