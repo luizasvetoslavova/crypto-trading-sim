239833b503ca6851bf4b18ec24a6c740b480c162
Luiza Svetoslavova <luizasvetoslavova16@gmail.com>
2025-05-24 14:23:50 +0300

    add react frontend

--- backend/src/main/java/com/cryptosim/backend/CryptoTradingSimApplication.java
+++ backend/src/main/java/com/cryptosim/backend/CryptoTradingSimApplication.java
@@ -2,12 +2,12 @@ package com.cryptosim.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CryptoTradingSimApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoTradingSimApplication.class, args);
 	}
-
 }
