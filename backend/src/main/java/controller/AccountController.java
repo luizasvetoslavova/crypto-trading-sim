ed64c2abf4c9424a7e6a477194edb36f965beb46
Luiza Svetoslavova <luizasvetoslavova16@gmail.com>
2025-05-25 01:00:15 +0300

    implement repositories and controllers

--- backend/src/main/java/controller/AccountController.java
+++ backend/src/main/java/controller/AccountController.java
@@ -1,4 +1,56 @@
 package controller;
 
+import lombok.AllArgsConstructor;
+import model.Balance;
+import model.Holding;
+import model.Transaction;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@CrossOrigin(origins = "http://localhost:3000")
+@AllArgsConstructor
 public class AccountController {
+    private final BalanceRepository balanceRepo;
+    private final HoldingsRepository holdingsRepo;
+    private final TransactionsRepository transactionRepo;
+
+    @GetMapping("/balance")
+    public Balance getBalance() {
+        return balanceRepo.getBalance();
+    }
+
+    @PostMapping("/balance")
+    public void updateBalance(@RequestBody Balance balance) {
+        balanceRepo.updateBalance(balance);
+    }
+
+    @GetMapping("/holdings")
+    public List<Holding> getHoldings() {
+        return holdingsRepo.getAllHoldings();
+    }
+
+    @PostMapping("/holdings")
+    public void upsertHolding(@RequestBody Holding holding) {
+        holdingsRepo.upsertHolding(holding);
+    }
+
+    @GetMapping("/transactions")
+    public List<Transaction> getTransactions() {
+        return transactionRepo.getAllTransactions();
+    }
+
+    @PostMapping("/transactions")
+    public void insertTransaction(@RequestBody Transaction transaction) {
+        transactionRepo.insertTransaction(transaction);
+    }
+
+    @PostMapping("/reset")
+    public void resetAll() {
+        balanceRepo.resetBalance();
+        holdingsRepo.resetHoldings();
+        transactionRepo.resetTransactions();
+    }
 }
--- backend/src/main/java/controller/BalanceRepository.java
+++ backend/src/main/java/controller/BalanceRepository.java
@@ -1,4 +1,25 @@
 package controller;
 
+import lombok.AllArgsConstructor;
+import model.Balance;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@AllArgsConstructor
 public class BalanceRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public Balance getBalance() {
+        Double amount = jdbcTemplate.queryForObject("SELECT amount FROM balance LIMIT 1", Double.class);
+        return new Balance(amount);
+    }
+
+    public void updateBalance(Balance balance) {
+        jdbcTemplate.update("UPDATE balance SET amount = ?", balance.getAmount());
+    }
+
+    public void resetBalance() {
+        jdbcTemplate.update("UPDATE balance SET amount = 10000");
+    }
 }
